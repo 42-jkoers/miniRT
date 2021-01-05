de18ca43633ac622be452de1fe99a5d4c3648d2a
SirMorfield <joppe.koers@gmail.com>
2021-01-05 14:15:44 +0100

    move files

---
+++ src/ray.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ray.c                                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jkoers <jkoers@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/01/05 13:18:30 by jkoers        #+#    #+#                 */
+/*   Updated: 2021/01/05 13:35:20 by jkoers        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "gui.h"
+#include "shapes.h"
+#include "constants.h"
+#include "../lib/minilibx-linux/mlx.h"
+#include "../lib/ft_printf/ft_printf.h"
+#include "../lib/libft/include/libft.h"
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
