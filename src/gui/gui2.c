099e945e19d123cc647b53bac0e203eb0b27ecd4
SirMorfield <joppe.koers@gmail.com>
2021-03-02 23:38:44 +0100

    not worth the message

--- src/gui/gui2.c
+++ src/gui/gui2.c
@@ -12,15 +12,17 @@
 
 #include "gui.h"
 #include "constants.h"
-#include "parse_rt.h"
-#include "../lib/minilibx-linux/mlx.h"
-#include "ray.h"
+#include <stdio.h>
 
 void	gui_render_next_camera(t_gui *gui)
 {
+	if (gui->cameras->length == 1)
+		return ;
 	gui->camera_i++;
 	if (gui->camera_i >= gui->cameras->length)
 		gui->camera_i = 0;
+	if (VERBOSE)
+		printf("Switching to camera %lu\n", gui->camera_i + 1);
 	ft_memcpy(
 		&gui->camera, arr_get(gui->cameras, gui->camera_i), sizeof(t_camera));
 	gui_display_scene(gui);
